076dcc0170cbcd5657a21d845954a306d7703cfb
Martin Hristov <tran4o@gmail.com>
2016-12-07 02:40:20 +0200

    version touch

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ module.exports= {
 	"server" : 
 	[
 		{
-			"file" : "server-6.1.3-all.zip",
-			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/1.0.0/server-6.1.3-all.zip",
-			"version" : "6.1.3"
+			"file" : "server-6.1.4-all.zip",
+			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/1.0.0/server-6.1.4-all.zip",
+			"version" : "6.1.4"
 		}
 	],
 	"import" : [
