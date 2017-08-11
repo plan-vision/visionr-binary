4b157223944dfbc13e9303e35961f41ca3f17652
Miroslav Penev <miroslav.penev@plan-vision.com>
2017-08-11 17:11:39 +0200

    New version 6.6.9

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ module.exports= {
 			"platform" : "win32",
 			"arch" : "x64",
 			"file" : "jre1.8.0_112-win64.zip",
-			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.5/jre1.8.0_112-win64.zip",
+			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.9/jre1.8.0_112-win64.zip",
 			"version" : "1.8.8_112"
 		},
 		{
 			"platform" : "win32",
 			"arch" : "ia32",
 			"file" : "jre1.8.0_112-win32.zip",
-			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.5/jre1.8.0_112-win32.zip",
+			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.9/jre1.8.0_112-win32.zip",
 			"version" : "1.8.8_112"
 		}
 		
@@ -20,7 +20,7 @@ module.exports= {
 		{
 			"platform" : "win32",
 			"file" : "libreoffice5.2.2-win.zip",
-			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.5/libreoffice5.2.2-win.zip",
+			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.9/libreoffice5.2.2-win.zip",
 			"version" : "5.2.2" 
 		}
 		
@@ -28,9 +28,9 @@ module.exports= {
 	"server" : 
 	[
 		{
-			"file" : "server-170802-all.zip",
-			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.5/server-170802-all.zip",
-			"version" : "6.6.5"
+			"file" : "server-170811-all.zip",
+			"url"  : "https://github.com/plan-vision/visionr-binary/releases/download/6.6.9/server-170811-all.zip",
+			"version" : "6.6.9"
 		}
 	],
 	"import" : [
