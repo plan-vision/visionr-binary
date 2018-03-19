f25379b52b805ef6d51c3a7dc54f4ec4208deac6
Martin Hristov <tran4o@gmail.com>
2018-03-19 19:09:30 +0200

    fix

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ module.exports = {
 		{
 			"platform" : "win32",
 			"arch" : "x64",
-			"file" : "jre1.8.0_112-win64.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre1.8.0_112-win64.zip",
-			"version" : "1.8.8_112"
+			"file" : "jre-9.0.4-win64.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre-9.0.4-win64.zip",
+			"version" : "9.0.4"
 		},
 		{
 			"platform" : "win32",
 			"arch" : "ia32",
-			"file" : "jre1.8.0_112-win32.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre1.8.0_112-win32.zip",
-			"version" : "1.8.8_112"
+			"file" : "jre-9.0.4-win32.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre-9.0.4-win32.zip",
+			"version" : "9.0.4"
 		}
 	],
 	"libreoffice" : [	
 		{
 			"platform" : "win32",
-			"file" : "libreoffice5.2.2-win.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/libreoffice5.2.2-win.zip",
-			"version" : "5.2.2" 
+			"file" : "libreoffice-6.0.2-win.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/libreoffice-6.0.2-win.zip",
+			"version" : "6.0.2" 
 		}
 		
 	]
