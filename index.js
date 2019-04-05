6b97f13cfef4c581d0d1b107c5dbe766987b549b
Miroslav Penev <miroslav.penev@plan-vision.com>
2019-04-05 09:03:18 +0200

    JRE to version 11.0.1

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = {
 		{
 			"platform" : "win32",
 			"arch" : "x64",
-			"file" : "jre-9.0.4-win64.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre-9.0.4-win64.zip",
-			"version" : "9.0.4"
+			"file" : "jre-11.0.1-win64.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre-11.0.1-win64.zip",
+			"version" : "11.0.1"
 		},
 		{
 			"platform" : "win32",
