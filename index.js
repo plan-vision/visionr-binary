d265f310a98bfc35dab1d0bcc79c17f3dc910caf
Miroslav Penev <miroslav.penev@plan-vision.com>
2020-01-13 11:38:45 +0100

    13.01.2021:F70016. Forms - set font-size in tree to 13px for all node elements

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ module.exports = {
 		{
 			"platform" : "win32",
 			"arch" : "x64",
-			"file" : "jre-11.0.1-win64.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre-11.0.1-win64.zip",
-			"version" : "11.0.1"
+			"file" : "jre-13.0.1-win64.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/7/jre-13.0.1-win64.zip",
+			"version" : "13.0.1"
 		},
 		{
 			"platform" : "win32",
 			"arch" : "ia32",
 			"file" : "jre-9.0.4-win32.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/jre-9.0.4-win32.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/7/jre-9.0.4-win32.zip",
 			"version" : "9.0.4"
 		}
 	],
@@ -19,7 +19,7 @@ module.exports = {
 		{
 			"platform" : "win32",
 			"file" : "libreoffice-6.0.2-win.zip",
-			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/6/libreoffice-6.0.2-win.zip",
+			"url"  : "https://github.com/plan-vision/visionr-requirements/releases/download/7/libreoffice-6.0.2-win.zip",
 			"version" : "6.0.2" 
 		}
 		
